dc790a489d55d9970c6c8a0c4b2e3e9abbef3d03
Marianne <mariannexpiquet@gmail.com>
2020-05-22 20:17:03 +0200

    ajout navbar component & bootstrap cnd

--- content/themes/recipes/recipes-front/src/App.js
+++ content/themes/recipes/recipes-front/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Recipes from './components/Recipes';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div >
+      <Navbar />
       <Recipes />
     </div>
   );
---
+++ content/themes/recipes/recipes-front/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">Recette de Bot</a>
+                <div className="collapse navbar-collapse" id="navbarColor02">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Accueil<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Recettes</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Galerie</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Concours</a>
+                        </li>
+                    </ul>   
+                </div> 
+            </nav>
+        )
+    }
+}
+
+export default Navbar
