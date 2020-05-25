bb971997a7e46fab51755d497e8517348fae5d02
Marianne <mariannexpiquet@gmail.com>
2020-05-25 21:12:13 +0200

    plugin cpt - taxo : function ajout des taxonomies reliés aux cpt recettes

--- content/plugins/cpt-recettes/cpt-recettes.php
+++ content/plugins/cpt-recettes/cpt-recettes.php
@@ -17,6 +17,8 @@ class CptRecipes
     public function __construct() 
     {
         add_action('init', [$this, 'register_cpt_recipes']);
+        add_action('init', [$this, 'recipes_register_taxonomies']);
+
         
     }
 
@@ -60,14 +62,38 @@ class CptRecipes
 
 
         register_post_type("recettes", $args);
+
+    }
+
+    public function recipes_register_taxonomies() 
+    {
+        // Création d'une taxo pour relié les recettes par catégories de recettes
+        register_taxonomy(
+            'recettes',
+            'recettes', // On relit la taxonomy sur notre post_type
+            [
+                'labels' => [
+                    'name' => 'Recettes',
+                    'singular_name' => 'Recette'
+                ],
+                'public' => true,
+                'hierarchical' => true, 
+                'show_admin_column' => true,
+                'rewrite' => [
+                    'slug' => 'recettes'
+                ]
+            ]
+        );
     }
 
     // re-calcul des routes à l'activation et la désactivation du plugin 
     // Pour éviter d'aller enregistrer les permaliens à chaque nouveau post du cpt
     public function recipes_activate()
     {
-        // Exécute la méthode du plugin qui permet de décalerre le cpt à WP
+        // Exécute la méthode du plugin qui permet de décaler le cpt à WP
         $this->register_cpt_recipes();
+        // pareil pour la taxonomie
+        $this->recipes_register_taxonomies();
         // Exécute la fonction native de WP qui permet de recalculer les routes et les droits
         flush_rewrite_rules();
     }
@@ -84,4 +110,4 @@ $cptRecipes = new CptRecipes();
 
  register_activation_hook(__FILE__, [$cptRecipes, 'recipes_activate']);
 
- register_deactivation_hook(__FILE__, [$cptRecipes, 'recipes_desactivate']);
+ register_deactivation_hook(__FILE__, [$cptRecipes, 'recipes_desactivate']); 
