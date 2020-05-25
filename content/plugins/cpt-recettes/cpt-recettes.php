e5d5c05e73e66219ebb08cfba016fbd342b0ed53
Marianne <mariannexpiquet@gmail.com>
2020-05-25 18:50:12 +0200

    resolution du plugin cpt recettes

--- content/plugins/cpt-recettes/cpt-recettes.php
+++ content/plugins/cpt-recettes/cpt-recettes.php
@@ -4,7 +4,7 @@
 Plugin Name: Custom Post Type Recettes
 Description : Création d'un CPT pour rassembler nos recettes
 Author: Marianne
-Version: 0.1
+Version: 1.0.0
 */
 
 // Source pour création de Custom Post Field : https://codex.wordpress.org/Function_Reference/register_post_type
@@ -12,56 +12,101 @@ Version: 0.1
 // Si pas de WPINC => On stop le script
 if (!defined('WPINC')) {die();}
 
-class CptRecipes
-{
-    public function __constructor() 
-    {
-        add_action('init', [$this, 'register_cpt_recipes']);
+// class CptRecipes
+// {
+//     public function __constructor() 
+//     {
+//         add_action('init', [$this, 'register_cpt_recipes']);
         
-    }
+//     }
 
-    public function register_cpt_recipes() 
-    {
-        $labels = [
-            "name"                  => "Recettes",
-            "singular_name"         => "Recette",
-            "menu_name"             => "Recettes",
-            "name_admin_bar"        => "Recette",
-            "add_new"               => "Ajouter une recette",
-            "add_new_item"          => "Ajouter une nouvelle recette",
-            "new_item"              => "Nouvelle recette",
-            "edit_item"             => "Editer la recette",
-            "view_item"             => "Voir la recette", 
-            "all_items"             => "Voir toutes les recettes",
-            "search_items"          => "Rechercher une recette",
-            "not_found"             => "Aucune recette trouvée",
-            "not_found_in_trash"    => "Aucune recette trouvée dans la corbeille",
-            "featured_media"        => "Image de la recette",
-            "set_featured_media"    => "Définir l'image de la recette",
-            "remove_featured_image" => "Retirer l'image de la recette",
-            "use_featured_media"    => "Utiliser l'image de la recette"
-        ];
+//     public function register_cpt_recipes() 
+//     {
+//         $labels = [
+//             "name"                  => "Recettes",
+//             "singular_name"         => "Recette",
+//             "menu_name"             => "Recettes",
+//             "name_admin_bar"        => "Recette",
+//             "add_new"               => "Ajouter une recette",
+//             "add_new_item"          => "Ajouter une nouvelle recette",
+//             "new_item"              => "Nouvelle recette",
+//             "edit_item"             => "Editer la recette",
+//             "view_item"             => "Voir la recette", 
+//             "all_items"             => "Voir toutes les recettes",
+//             "search_items"          => "Rechercher une recette",
+//             "not_found"             => "Aucune recette trouvée",
+//             "not_found_in_trash"    => "Aucune recette trouvée dans la corbeille",
+//             "featured_media"        => "Image de la recette",
+//             "set_featured_media"    => "Définir l'image de la recette",
+//             "remove_featured_image" => "Retirer l'image de la recette",
+//             "use_featured_media"    => "Utiliser l'image de la recette"
+//         ];
 
-        $args = [
-            "labels"        => $labels,
-            "public"        => true, 
-            "menu_position" => 5,
-            "menu_icon"     => "dashicons-carrot",
-            "hierarchical"  => false,
-            "supports"      => [
-                "title",
-                "editor",
-                "author",
-                "thumbnail",
-                "excerpt",
-                "custom-fields"
-            ]
-        ];
+//         $args = [
+//             "labels"        => $labels,
+//             "public"        => true, 
+//             "menu_position" => 5,
+//             "menu_icon"     => "dashicons-carrot",
+//             "hierarchical"  => false,
+//             "supports"      => [
+//                 "title",
+//                 "editor",
+//                 "author",
+//                 "thumbnail",
+//                 "excerpt",
+//                 "custom-fields"
+//             ]
+//         ];
 
 
-        register_post_type("recipes", $args);
-    }
+//         register_post_type("recipes", $args);
+//     }
+// }
+
+// $cptRecipes = new CptRecipes();
+
+// La class ne fonctionne pas
+
+function register_cpt_recipes() 
+{
+    $labels = [
+        "name"                  => "Recettes",
+        "singular_name"         => "Recette",
+        "menu_name"             => "Recettes",
+        "name_admin_bar"        => "Recette",
+        "add_new"               => "Ajouter une recette",
+        "add_new_item"          => "Ajouter une nouvelle recette",
+        "new_item"              => "Nouvelle recette",
+        "edit_item"             => "Editer la recette",
+        "view_item"             => "Voir la recette", 
+        "all_items"             => "Voir toutes les recettes",
+        "search_items"          => "Rechercher une recette",
+        "not_found"             => "Aucune recette trouvée",
+        "not_found_in_trash"    => "Aucune recette trouvée dans la corbeille",
+        "featured_media"        => "Image de la recette",
+        "set_featured_media"    => "Définir l'image de la recette",
+        "remove_featured_image" => "Retirer l'image de la recette",
+        "use_featured_media"    => "Utiliser l'image de la recette"
+    ];
+
+    $args = [
+        "labels"        => $labels,
+        "public"        => true, 
+        "menu_position" => 5,
+        "menu_icon"     => "dashicons-carrot",
+        "hierarchical"  => false,
+        "supports"      => [
+            "title",
+            "editor",
+            "author",
+            "thumbnail",
+            "excerpt",
+            "custom-fields"
+        ]
+    ];
+
+
+    register_post_type("recettes", $args);
 }
 
-$cptRecipes = new CptRecipes;
-
+add_action('init', 'register_cpt_recipes');
--- content/themes/recipes/index.php
+++ content/themes/recipes/index.php
@@ -1,47 +1,52 @@
+
+<!-- HEADER -->
 <?php get_header() ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Recette de bot</title>
+  
 </head>
-<body>
-<!-- HEADER -->
-  <header class="header">
-    <nav class="navbar">
-      <a href="#">Accueil</a>
-      <a href="#">Recettes</a>
-      <a href="#">Galerie</a>
-      <a href="#">Concours</a>
-    </nav>
-  </header>
 
-<!-- MAIN -->
-  <main class="container"> 
-    <article>
-      <h1>Nom de la recette</h1>
-      <img src="#" alt="image-recipe">
-    </article>
-    <article>
-      <h1>Nom de la recette</h1>
-      <img src="#" alt="image-recipe">
-    </article>
-    <article>
-      <h1>Nom de la recette</h1>
-      <img src="#" alt="image-recipe">
-    </article>
-  </main>
+  <body>
+  <div class="wrapper">
+      <header class="header">
+        <nav class="navbar">
+          <a href="#">Accueil</a>
+          <a href="#">Recettes</a>
+          <a href="#">Galerie</a>
+          <a href="#">Concours</a>
+        </nav>
+      </header>
 
+    <!-- MAIN -->
+      <main class="container"> 
+        <article>
+          <h1>Nom de la recette</h1>
+          <img src="#" alt="image-recipe">
+        </article>
+        <article>
+          <h1>Nom de la recette</h1>
+          <img src="#" alt="image-recipe">
+        </article>
+        <article>
+          <h1>Nom de la recette</h1>
+          <img src="#" alt="image-recipe">
+        </article>
+      </main>
 
-<!-- FOOTER -->
-  <footer class="footer">
-    <div>
-      <h4>Site Recette de Bot créé par Marianne PN</h4>
+    <!-- FOOTER -->
+
+    <footer class="footer">
+        <div>
+          <h4>Site Recette de Bot créé par Marianne PN</h4>
+        </div>
+      </footer>
     </div>
-  </footer>
-</body>
+  </body>
 </html>
-
+   
 <?php get_footer() ?>
